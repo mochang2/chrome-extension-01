c492d9772825da63f0e18bfc0464965bb1da7442
changmolee <changmolee@netmable.com>
2024-04-24 14:15:10 +0900

    feat: 파이프라인 실행 변수 저장 및 자동 입력 기능 구현

---
+++ scripts/popup.js
@@ -0,0 +1,137 @@
+const STORAGE_KEY = "last_inputs";
+
+function changeLoading() {
+    const loading = document.querySelector(".show");
+    const content = document.querySelector(".hide");
+
+    loading.classList.remove("show");
+    loading.classList.add("hide");
+
+    content.classList.remove("hide");
+    content.classList.add("show");
+}
+
+function getList() {
+    return document.querySelector(".list");
+}
+
+function getInputs(key, value) {
+    const keyInputCell = document.createElement("td");
+    const keyInput = document.createElement("input");
+    keyInput.setAttribute("type", "text");
+    keyInput.setAttribute("value", key ?? "");
+    keyInput.classList.add("key");
+    keyInputCell.appendChild(keyInput);
+
+    const valueInputCell = document.createElement("td");
+    const valueInput = document.createElement("input");
+    valueInput.setAttribute("type", "text");
+    valueInput.setAttribute("value", value ?? "");
+    valueInput.classList.add("value");
+    valueInputCell.appendChild(valueInput);
+
+    const deleteButtonCell = document.createElement("td");
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete");
+    deleteButton.innerText = "-";
+    deleteButton.addEventListener("click", () => {
+        wrapper.remove();
+    });
+    deleteButtonCell.appendChild(deleteButton);
+
+    const wrapper = document.createElement("tr");
+
+    wrapper.appendChild(keyInputCell);
+    wrapper.appendChild(valueInputCell);
+    wrapper.appendChild(deleteButtonCell);
+
+    return wrapper;
+}
+
+function appendInputsToList(key, value) {
+    const list = getList();
+    list.appendChild(getInputs(key, value));
+}
+
+async function submit() {
+    async function writeInputs() {
+        const [tab] = await chrome.tabs.query({
+            active: true,
+            currentWindow: true,
+        });
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            args: [inputs],
+            func: (variables) => {
+                const inputList = document.querySelector("ul.ci-variable-list");
+                let emptyLastInput = inputList.querySelector("li");
+
+                for (const [key, value] of variables) {
+                    const keyInputs = document.querySelectorAll(
+                        ".js-ci-variable-input-key.ci-variable-body-item.qa-ci-variable-input-key.form-control"
+                    );
+                    const lastKeyInput = keyInputs[keyInputs.length - 1];
+                    const valueInputs = document.querySelectorAll(
+                        ".form-control.js-ci-variable-input-value.js-secret-value.qa-ci-variable-input-value"
+                    );
+                    const lastValueInput = valueInputs[valueInputs.length - 1];
+
+                    emptyLastInput = emptyLastInput.cloneNode(true);
+                    lastKeyInput.value = key;
+                    lastValueInput.value = value;
+                    inputList.appendChild(emptyLastInput);
+                }
+            },
+        });
+    }
+
+    async function store() {
+        await chrome.storage.local.set({ [STORAGE_KEY]: inputs });
+        console.log("Inputs are saved", inputs);
+    }
+
+    const inputs = [];
+    (function extractInputs() {
+        const lists = getList().getElementsByTagName("tr");
+        for (let i = 0; i < lists.length; i++) {
+            const key = lists[i].getElementsByClassName("key")[0].value;
+            const value = lists[i].getElementsByClassName("value")[0].value;
+
+            if (key === "" || value === "") {
+                continue;
+            }
+
+            inputs.push([key, value]);
+        }
+    })();
+
+    await writeInputs();
+    await store();
+}
+
+window.onload = async () => {
+    const result = await chrome.storage.local.get();
+
+    changeLoading();
+
+    const inputs = result[STORAGE_KEY] ?? [["", ""]];
+
+    const addButton = document.querySelector(".add");
+    addButton.addEventListener("click", () => {
+        appendInputsToList();
+    });
+
+    const submitButton = document.querySelector(".submit");
+    submitButton.addEventListener("click", submit);
+
+    const table = document.querySelector("table");
+    table.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+            submit();
+        }
+    });
+
+    for (const [key, value] of inputs) {
+        appendInputsToList(key, value);
+    }
+};
